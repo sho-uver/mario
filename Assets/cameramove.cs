aacdd2a1de39105aec708c9f4abbf78f0bc508f2
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-05-20 23:16:11 +0900

    プロジェクト作成

---
+++ Assets/cameramove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class cameramove: MonoBehaviour
+{
+    GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("マリオ");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+        
+    }
+}
